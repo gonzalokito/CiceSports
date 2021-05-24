2d0bcb2201493f206509b2599959c041a9d8a805
Gonzalo <gonzalokito91@gmail.com>
2021-05-24 21:32:03 +0200

    [WIP] - Added AppAssembly class and protocol | instance in AppDelegate of AppAssembly -

--- CiceSport/AppDelegate.swift
+++ CiceSport/AppDelegate.swift
@@ -10,10 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    let appAssembly:AppAssemblyProtocol = AppAssembly()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        //Variable temporal para comprobar que window no es nulo
+        if let windowDes = window {
+            appAssembly.setPrincipalViewController(in: windowDes)
+        }
         return true
     }
 
---
+++ CiceSport/Core/AppAssembly.swift
@@ -0,0 +1,26 @@
+//
+//  AppAssembly.swift
+//  CiceSport
+//
+//  Created by cice on 24/05/2021.
+//
+
+//Controlador de vista
+import Foundation
+import UIKit
+
+protocol AppAssemblyProtocol {
+    func setPrincipalViewController(in window: UIWindow)
+}
+
+class AppAssembly: AppAssemblyProtocol {
+    
+    var actualViewController: UIViewController!
+    
+    internal func setPrincipalViewController(in window: UIWindow){
+        
+        actualViewController = SplashViewController()
+        window.rootViewController = actualViewController
+        window.makeKeyAndVisible()
+    }
+}
