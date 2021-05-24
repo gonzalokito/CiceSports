ab1616f801cad82801d3513e74ad6bd46eb7249b
Gonzalo <gonzalokito91@gmail.com>
2021-05-24 20:41:08 +0200

    [WIP][J-001]  - Added new class and new folder

---
+++ CiceSport/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CiceSport
+//
+//  Created by cice on 24/05/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
