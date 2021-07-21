ff8cc3ab53b8fc1911f7a5ffb5c5f26661ccc979
Gonzalo Prieto Benito <gonzalokito91@gmail.com>
2021-07-21 17:55:25 +0200

    [WIP] Created CiceSport N1 Training

---
+++ CiceSport/Modules/Training/N1/TrainingOneViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingOneViewController.swift
+//  CiceSport
+//
+//  Created by cice on 20/07/2021.
+//
+
+import UIKit
+
+class TrainingOneViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
